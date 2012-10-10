c9fae41878206d62d9cce64c2c96751b54a49f0f
Asko Soukka <asko.soukka@iki.fi>
2012-10-10 23:31:41 +0300

    Added minimal test for consuming server.

--- src/collective/zamqp/tests/test_layer.py
+++ src/collective/zamqp/tests/test_layer.py
@@ -1,36 +1,63 @@
 # -*- coding: utf-8 -*-
 import unittest2 as unittest
 
+
 from collective.zamqp.testing import (
+    runAsyncTest,
     RABBIT_APP_FUNCTIONAL_TESTING,
     ZAMQP_FUNCTIONAL_TESTING,
-    )
+)
 
 
 class RabbitFunctional(unittest.TestCase):
 
     layer = RABBIT_APP_FUNCTIONAL_TESTING
 
-    def testNoQueues(self):
+    def _testNoQueues(self):
         rabbitctl = self.layer['rabbitctl']
         self.assertEqual('\n'.join(rabbitctl('list_queues')),
                          'Listing queues ...\n...done.\n\n')
+
+    def testNoQueues(self):
+        runAsyncTest(self._testNoQueues)
 
 
 class ZAMQPFunctional(unittest.TestCase):
 
     layer = ZAMQP_FUNCTIONAL_TESTING
 
-    def testNoQueues(self):
-        from collective.zamqp import connection
-        connection.connect_all()
+    def _testDeclareQueue(self):
+        rabbitctl = self.layer['rabbitctl']
+        self.assertIn("my.queue\t0",
+                      rabbitctl('list_queues')[0].split("\n"))
 
-        from zope.testing.loggingsupport import InstalledHandler
-        handler = InstalledHandler("collective.zamqp")
+    def testDeclareQueue(self):
+        runAsyncTest(self._testDeclareQueue)
 
-        import asyncore
-        asyncore.loop(timeout=1, count=10)
-        for record in handler.records: print record.getMessage()
-
+    def _testDeclareQueueAgain(self):
         rabbitctl = self.layer['rabbitctl']
         self.assertIn("my.queue\t0", rabbitctl('list_queues')[0].split("\n"))
+
+    def testDeclareQueueAgain(self):
+        runAsyncTest(self._testDeclareQueueAgain)
+
+    def _testPublishToQueue(self):
+        rabbitctl = self.layer['rabbitctl']
+        self.assertIn("my.queue\t1", rabbitctl('list_queues')[0].split("\n"))
+
+    def _testPublishToQueueAndConsumeIt(self):
+        rabbitctl = self.layer['rabbitctl']
+        self.assertIn("my.queue\t0", rabbitctl('list_queues')[0].split("\n"))
+
+    def testPublishToQueueAndConsumeIt(self):
+        from zope.component import getUtility
+        from collective.zamqp.interfaces import IProducer
+        producer = getUtility(IProducer, name="my.queue")
+        producer.publish("Hello world!")
+
+        runAsyncTest(self._testPublishToQueue)
+        runAsyncTest(self._testPublishToQueueAndConsumeIt)
+
+        # from zope.testing.loggingsupport import InstalledHandler
+        # handler = InstalledHandler("collective.zamqp")
+        # for record in handler.records: print record.getMessage()

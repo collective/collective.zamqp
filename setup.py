81a2bb33ce7c8a52434f3bc9701e0321b00c6155
Asko Soukka <asko.soukka@iki.fi>
2013-11-08 14:14:18 +0200

    Back to development: 0.13.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.zamqp',
-    version='0.13.0',
+    version='0.13.1.dev0',
     description="Asynchronous AMQP-integration for Plone (and Zope2)",
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.txt").read()),
